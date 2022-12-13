27298b18ad3c1ee67b3e532bef1d20b78163856d
excelsior091224 <excelsior091224@gmail.com>
2022-12-13 17:02:43 +0900

    edit microcms.ts

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -1,6 +1,4 @@
 import { createClient, MicroCMSQueries } from "microcms-js-sdk";
-console.log(import.meta.env.MICROCMS_API_KEY);
-console.log(import.meta.env.MICROCMS_SERVICE_DOMAIN);
 const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: import.meta.env.MICROCMS_API_KEY,
@@ -28,12 +26,20 @@ export type BlogResponse = {
 };
 
 export const getBlogs = async (queries?: MicroCMSQueries) => {
+  const client = createClient({
+    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: import.meta.env.MICROCMS_API_KEY,
+  });
   return await client.get<BlogResponse>({ endpoint: "blogs", queries });
 };
 export const getBlogDetail = async (
   contentId: string,
   queries?: MicroCMSQueries
 ) => {
+  const client = createClient({
+    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: import.meta.env.MICROCMS_API_KEY,
+  });
   return await client.getListDetail<Blog>({
     endpoint: "blogs",
     contentId,
