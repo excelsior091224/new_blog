7d6c881997b4c22cfdafcdb4c82bc21bbcf77e05
excelsior091224 <excelsior091224@gmail.com>
2022-12-23 10:53:36 +0900

    pagenation in category

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -32,6 +32,20 @@ export type BlogResponse = {
   limit: number;
   contents: Blog[];
 };
+export type Category = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+};
+export type CategoryResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Category[];
+};
 
 export const getBlogs = async (queries?: MicroCMSQueries) => {
   // const client = createClient({
@@ -53,4 +67,11 @@ export const getBlogDetail = async (
     contentId,
     queries,
   });
+};
+export const getCategories = async (queries?: MicroCMSQueries) => {
+  // const client = createClient({
+  //   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  //   apiKey: import.meta.env.MICROCMS_API_KEY,
+  // });
+  return await client.get<CategoryResponse>({ endpoint: "categories", queries });
 };
