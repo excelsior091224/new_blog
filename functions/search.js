b4d1b765284a832ad5ef5b9c7ab8ced895d484f4
excelsior091224 <excelsior091224@gmail.com>
2022-12-28 18:50:24 +0900

    add search.js

--- functions/search.js
+++ functions/search.js
@@ -1,15 +1,15 @@
 // functions/search.js
-
-//const { client } = require('../src/library/microcms');
 import { createClient, MicroCMSQueries } from "microcms-js-sdk";
-const client = createClient({
-  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: import.meta.env.MICROCMS_API_KEY,
-});
 
-// eslint-disable-next-line require-await
-exports.handler = async (event) => {
-  const { q } = event.queryStringParameters;
+export function onRequest(context) {
+  //const { client } = require('../src/library/microcms');
+  
+  const client = createClient({
+    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: import.meta.env.MICROCMS_API_KEY,
+  });
+
+  const { q } = context.queryStringParameters;
   if (!q) {
     return {
       statusCode: 400,
