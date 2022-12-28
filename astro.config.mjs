0221f1e2e3b2f7916d130f84b0e2dd00b6a5ef8e
excelsior091224 <excelsior091224@gmail.com>
2022-12-28 17:00:38 +0900

    remove cloudflare adoper

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,8 @@ import netlify from "@astrojs/netlify/functions";
 export default defineConfig({
   integrations: [mdx(), sitemap()],
   site: 'https://new-blog-81t.pages.dev',
-  output: "server",
-  adapter: cloudflare(),
+//  output: "server",
+//  adapter: cloudflare(),
   vite: {
     define: {
         "process.env.MICROCMS_SERVICE_DOMAIN": process.env.MICROCMS_SERVICE_DOMAIN,
