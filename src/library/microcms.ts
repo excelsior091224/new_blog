e21a4b1fc6e9c4c5b256ed4de2e1e406f88ce387
excelsior091224 <excelsior091224@gmail.com>
2022-12-28 16:11:26 +0900

    env config test 2

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -19,12 +19,34 @@ export type Blog = {
     height: number;
     width: number;
   };
+  category: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    name: string;
+  };
 };
 export type BlogResponse = {
   totalCount: number;
   offset: number;
   limit: number;
   contents: Blog[];
+};
+export type Category = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+};
+export type CategoryResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Category[];
 };
 
 export const getBlogs = async (queries?: MicroCMSQueries) => {
@@ -44,6 +66,27 @@ export const getBlogDetail = async (
 //  });
   return await client.getListDetail<Blog>({
     endpoint: "blogs",
+    contentId,
+    queries,
+  });
+};
+export const getCategories = async (queries?: MicroCMSQueries) => {
+  // const client = createClient({
+  //   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  //   apiKey: import.meta.env.MICROCMS_API_KEY,
+  // });
+  return await client.get<CategoryResponse>({ endpoint: "categories", queries });
+};
+export const getCategoryDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  // const client = createClient({
+  //   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  //   apiKey: import.meta.env.MICROCMS_API_KEY,
+  // });
+  return await client.getListDetail<Category>({
+    endpoint: "categories",
     contentId,
     queries,
   });
