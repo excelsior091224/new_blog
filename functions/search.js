405fe853c3ddd59fbc1932afa3e6a1dd736f13ea
excelsior091224 <excelsior091224@gmail.com>
2022-12-29 13:26:33 +0900

    edit search.js

--- functions/search.js
+++ functions/search.js
@@ -1,14 +1,13 @@
 // functions/search.js
 import { createClient } from "microcms-js-sdk";
 
-export async function onRequest({ request }) {
+export async function onRequest({ request, env }) {
   const client = createClient({
-    serviceDomain: "import.meta.env.MICROCMS_SERVICE_DOMAIN",
-    apiKey: "import.meta.env.MICROCMS_API_KEY",
+    serviceDomain: env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: env.MICROCMS_API_KEY,
   });
 
   const url = request.url;
-  // const { q } = request.query;
   const q  = new URL(url).searchParams.get('q');
   if (!q) {
     return new Response(JSON.stringify({
@@ -25,7 +24,7 @@ export async function onRequest({ request }) {
       return new Response(JSON.stringify(data), {status:200})
     })
     .catch((error) => (
-      // new Response(String(error), {status:400})
-      new Response(JSON.stringify({error:String(error),query:q}), {status:400})
+      new Response(String(error), {status:400})
+      // new Response(JSON.stringify({error:String(error),query:q,client:client}), {status:400})
     ));
 };
