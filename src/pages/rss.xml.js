85265fe32f98ec61e7d74377731360b72c7d329b
excelsior091224 <excelsior091224@gmail.com>
2023-01-10 17:04:25 +0900

    changed about rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,20 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import { getBlogs } from "../library/microcms";
+const { contents: posts } = await getBlogs({ fields: ["id", "title", "publishedAt","content","eyecatch","category"],orders:'-publishedAt'});
 
 export const get = () =>
 	rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./**/*'),
+		items: posts.map(post => {
+			return {
+				link:`/posts/${post.id}`,
+			  	pubDate: post.publishedAt,
+			  	title: post.title,
+				description: post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').length > 100 ? post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').slice(0,101) + '...' : post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,''),
+				content:post.content
+			}
+		  })
 	});
