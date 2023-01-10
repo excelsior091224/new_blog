fd5c02b5cb1624abb5d35b7cca864876834b716f
excelsior091224 <excelsior091224@gmail.com>
2023-01-10 15:41:10 +0900

    edited about partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,25 +10,23 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 import vue from "@astrojs/vue";
-
 import partytown from "@astrojs/partytown";
-
 import preact from "@astrojs/preact";
-
 import robotsTxt from 'astro-robots-txt';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap(), vue(), partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    preact({ compat: true }),
-    robotsTxt()
-  ],
-  site: 'https://www.vermilion3.xyz',
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), preact({
+    compat: true
+  }), robotsTxt()],
+  site: 'https://www.vermilion3.xyz'
   // output: "server",
   // adapter: cloudflare(),
   // vite: {
