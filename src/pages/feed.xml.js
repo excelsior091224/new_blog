ad5d1e1f1ecc1d4382cc4e1fe33f7eb69e79910a
excelsior091224@gmail.com <excelsior091224@gmail.com>
2023-02-14 10:50:23 +0900

    rss feed test & add feedly button

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -22,6 +22,7 @@ export const get = () =>
                 .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
                 .slice(0, 101) + "..."
             : post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""),
+        customData: post.eyecatch && `<enclosure url=${post.eyecatch.url}> </enclosure>`,
       };
     }),
   });
