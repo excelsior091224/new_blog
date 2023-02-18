02402323ed84a6fcb87a1ab4efcc122a0601000c
excelsior091224 <excelsior091224@gmail.com>
2023-02-19 00:37:00 +0900

    feed.xml.js changed

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -22,7 +22,7 @@ export const get = () =>
                 .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
                 .slice(0, 101) + "..."
             : post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""),
-        customData: `<enclosure url="${post.eyecatch ? post.eyecatch.url : ""}"></enclosure>`,
+        customData: [`<enclosure url="${post.eyecatch ? post.eyecatch.url + "?fm=webp&fit=crop&crop=top&w=720&h=360" : ""}"></enclosure>`].join(''),
       };
     }),
   });
