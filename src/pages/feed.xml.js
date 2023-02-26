21a33e651717e0ebf69d3b84562d42d3445d8682
excelsior091224 <excelsior091224@gmail.com>
2023-02-26 22:17:46 +0900

    edited feed.xml.js

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -23,8 +23,7 @@ export const get = () =>
                 .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
                 .slice(0, 101) + "..."
             : post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""),
-        // customData: [`<enclosure url="${post.eyecatch ? post.eyecatch.url + "?fm=webp&fit=crop&crop=top&w=720&h=360" : ""}">eyecatch</enclosure>`].join(''),
-        customData: [`<enclosure url="${post.eyecatch ? post.eyecatch.url : ""}">eyecatch</enclosure>`].join(''),
+        // customData: [`<enclosure url="${post.eyecatch ? post.eyecatch.url : ""}">eyecatch</enclosure>`].join(''),
       };
     }),
   });
