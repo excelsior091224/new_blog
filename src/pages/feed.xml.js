1a82371d10ce1b09ac8a8e5082770dce5b4aa165
gimlet <44745901+excelsior091224@users.noreply.github.com>
2023-05-09 18:16:28 +0900

    update feed.xml.js

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,11 +1,6 @@
 import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { cmsBlog } from "../library/microcms";
-const { contents: posts } = await cmsBlog.getBlogs({
-  fields: ["id", "title", "publishedAt", "content", "eyecatch", "category"],
-  orders: "-publishedAt",
-});
-
 import http from 'http';
 import { parse } from 'url';
 
@@ -33,25 +28,35 @@ async function getImageFileSize(imageUrl) {
   });
 }
 
-export const get = () =>
-  rss({
+export const get = async () => {
+  const { contents: posts } = await cmsBlog.getBlogs({
+    fields: ["id", "title", "publishedAt", "content", "eyecatch", "category"],
+    orders: "-publishedAt",
+  });
+
+  const items = await Promise.all(posts.map(async (post) => {
+    const enclosureUrl = post.eyecatch ? `${post.eyecatch.url}?fit=crop&crop=top&w=720&h=360` : "";
+    const enclosureLength = await getImageFileSize(enclosureUrl);
+    return {
+      link: `/posts/${post.id}`,
+      pubDate: post.publishedAt,
+      title: post.title,
+      description:
+        post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "").length > 100
+          ? post.content
+              .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
+              .slice(0, 101) + "..."
+          : post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""),
+      enclosure: { url: enclosureUrl, length: enclosureLength, type: "image/png" }
+      // customData: [`<enclosure url="${post.eyecatch ? post.eyecatch.url : ""}">eyecatch</enclosure>`].join(''),
+    };
+  }));
+
+  return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
     customData: `<language>ja</language>`,
-    items: posts.map((post) => {
-      return {
-        link: `/posts/${post.id}`,
-        pubDate: post.publishedAt,
-        title: post.title,
-        description:
-          post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "").length > 100
-            ? post.content
-                .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
-                .slice(0, 101) + "..."
-            : post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""),
-        enclosure: {url: post.eyecatch ?await getImageFileSize(post.eyecatch.url)+ '?fit=crop&crop=top&w=720&h=360' : "",length:0,type: "image/png"}
-        // customData: [`<enclosure url="${post.eyecatch ? post.eyecatch.url : ""}">eyecatch</enclosure>`].join(''),
-      };
-    }),
+    items
   });
+};
