959c8523671baa6e1485128c53a6ee3304285da3
gimlet <44745901+excelsior091224@users.noreply.github.com>
2023-05-09 18:07:54 +0900

    update feed.xml.js

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -6,6 +6,33 @@ const { contents: posts } = await cmsBlog.getBlogs({
   orders: "-publishedAt",
 });
 
+import http from 'http';
+import { parse } from 'url';
+
+async function getImageFileSize(imageUrl) {
+  return new Promise((resolve) => {
+    const { hostname, path } = parse(imageUrl);
+
+    const options = {
+      method: 'HEAD',
+      hostname,
+      path,
+    };
+
+    const req = http.request(options, (res) => {
+      const fileSize = res.headers['content-length'];
+      resolve(parseInt(fileSize));
+    });
+
+    req.on('error', (error) => {
+      console.error(`エラーが発生しました: ${error}`);
+      resolve(1);
+    });
+
+    req.end();
+  });
+}
+
 export const get = () =>
   rss({
     title: SITE_TITLE,
@@ -23,7 +50,7 @@ export const get = () =>
                 .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
                 .slice(0, 101) + "..."
             : post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""),
-        enclosure: {url: post.eyecatch ? post.eyecatch.url + '?fit=crop&crop=top&w=720&h=360' : "",length:0,type: "image/png"}
+        enclosure: {url: post.eyecatch ?await getImageFileSize(post.eyecatch.url)+ '?fit=crop&crop=top&w=720&h=360' : "",length:0,type: "image/png"}
         // customData: [`<enclosure url="${post.eyecatch ? post.eyecatch.url : ""}">eyecatch</enclosure>`].join(''),
       };
     }),
