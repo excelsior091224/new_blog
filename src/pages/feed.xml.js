1e672067018cdb2087d88aaa9f381a7310d945ae
gimlet <44745901+excelsior091224@users.noreply.github.com>
2023-05-09 17:23:09 +0900

    update feed.xml.js

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -24,7 +24,7 @@ export const get = () =>
                 .slice(0, 101) + "..."
             : post.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""),
         enclosure: {
-         url: post.eyecatch ? post.eyecatch.url : "",
+         url: post.eyecatch ? post.eyecatch.url + '?fit=crop&crop=top&w=720&h=360' : "",
          length: 124568,
          type: "image/png"
         }
