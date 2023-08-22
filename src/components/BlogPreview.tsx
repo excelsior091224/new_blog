d147f30320840ce014a081e9ba1d9bd6537ae5e8
gimlet <44745901+excelsior091224@users.noreply.github.com>
2023-08-22 12:50:17 +0000

    html parse

--- src/components/BlogPreview.tsx
+++ src/components/BlogPreview.tsx
@@ -1,6 +1,10 @@
 import useSWR from "swr";
 import { createTableOfContents } from "microcms-richedit-processer";
 
+import { load } from "cheerio";
+import hljs, { HighlightResult } from "highlight.js";
+import 'highlight.js/styles/hybrid.css';
+
 const BlogPreview = () => {
   const params = new URLSearchParams(window.location.search);
   const contentId = params.get("contentId");
@@ -10,10 +14,57 @@ const BlogPreview = () => {
   const endpoint = contentId === null || draftKey === null ? null : `/api/preview?contentId=${contentId}&draftKey=${draftKey}`;
 
   const { data, error, isLoading, isValidating } = useSWR(
-    endpoint,fetcher
+    endpoint, fetcher
   );
 
-  const table_of_content = data?.content && createTableOfContents(data?.content,{ tags: "h1,h2,h3,h4,h5" })
+  if (error) return <div>エラーが発生しました</div>;
+
+  if (isLoading) return <div>読み込み中...</div>;
+
+  let modifiedContent = data?.content || "";
+
+  if (data && data.content) {
+    // APIから取得したリッチエディタのHTMLからcheerioオブジェクトを生成
+    const $ = load(data.content);
+
+    // コードブロックのファイル名が入力されている場合の処理
+    $("div[data-filename]").each((_, elm) => {
+      // data-filename属性の値を持つspanを
+      // <div data-filename="{入力したファイル名}">の最初の子要素として追加
+      $(elm).prepend(`<span class="filename">${$(elm).attr("data-filename")}</span>`);
+    });
+
+    // コードブロックのシンタックスハイライトを行う
+    $("pre code").each((_, elm) => {
+      const language = $(elm).attr("class") || "";
+      let result: HighlightResult;
+
+      if (language == "") {
+        // 言語が入力なしの場合、自動判定
+        result = hljs.highlightAuto($(elm).text());
+      } else {
+        // 言語が入力ありの場合、入力された言語で判定
+        result = hljs.highlight($(elm).text(), {
+          language: language.replace("language-", ""),
+        });
+      }
+      $(elm).html(result.value);
+      $(elm).addClass("hljs");
+    });
+
+    $("img").each((_, elm) => {
+      const src = $(elm).attr("src");
+      if (src && src.includes("https://images.microcms-assets.io/assets/")) {
+        const webpQueryParam = src.includes("?") ? "&fm=webp" : "?fm=webp";
+        $(elm).attr("src", src + webpQueryParam);
+      }
+    });
+
+    // 編集したHTMLを再設定
+    data.content = $.html();
+  }
+
+  const table_of_content = modifiedContent && createTableOfContents(modifiedContent, { tags: "h1,h2,h3,h4,h5" });
 
   if (error) return <div>エラーが発生しました</div>;
 
@@ -33,7 +84,7 @@ const BlogPreview = () => {
         <div class="table_of_content_wrapper">
           <h4 class="table_of_content_title">目次</h4>
           <ul class="table_of_content_lists">
-            {table_of_content.map((item:any) => (
+            {table_of_content.map((item: any) => (
               <li class={`table_of_content_list ${item.name}`}>
                 <a href={`#${item.id}`}>{item.text}</a>
               </li>
